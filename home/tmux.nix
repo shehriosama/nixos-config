845011ee9f60901b846fd6057c44d8b91038167c
shehriosama <ashehrigit19@gmail.com>
2025-10-06 13:42:09 +0300

    Add TMUX to home-manager

---
+++ home/tmux.nix
@@ -0,0 +1,12 @@
+{ lib, config, pkgs, ... }:
+
+{
+  programs.tmux = {
+    enable = true;
+    shell = "${pkgs.zsh}/bin/zsh";
+    terminal = "tmux-256color";
+    historyLimit = 100000;
+        #plugins = with pkgs;
+  };
+}
+
